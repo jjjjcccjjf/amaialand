53e692aa1f65b9915d282f7250b7811118e0a2d2
Lorenzo Salamante <lsalamante@myoptimind.com>
2017-06-28 10:53:28 +0800

    remove unused variable

--- getLocationsWithRFO.php
+++ getLocationsWithRFO.php
@@ -5,19 +5,19 @@ require_once("wp-load.php");
 
 if(isset($_POST['getLocationsWithRFO']) && $_POST['rfo'] == 1)
 {
-	
+
                 $args = array('post_type' => 'page', 'posts_per_page' => '-1',
                 'tax_query' => array(
-                  'relation' => 'AND',        
-                  array(          
-                  'taxonomy' => 'project_type',         
-                  'field'    => 'slug',         
+                  'relation' => 'AND',
+                  array(
+                  'taxonomy' => 'project_type',
+                  'field'    => 'slug',
                   'terms'    => array('condo','house-and-lot',' shophouse-development') )
-                  
+
                   )
-                  ); 
-               
-               
+                  );
+
+
 
               $query = new WP_Query( $args );
               $projects = $query->posts;
@@ -25,26 +25,24 @@ if(isset($_POST['getLocationsWithRFO']) && $_POST['rfo'] == 1)
               $options_wout_rfo = array();
               foreach ($projects as $proj) {
 
-                $proj_id = $proj->ID;
-
                 if(get_field('ready_for_occupation',$proj->ID) == 1)
-                {                  
-                  
+                {
+
                    $term_list = wp_get_post_terms($proj->ID, 'location', array("fields" => "all"));
-                   
-                   
+
+
                   foreach ($term_list as $terms) {
 
                     if($terms->parent != 0)
                      {
-                      
-                      
+
+
                       if(!in_array($terms->term_id,$options_with_rfo))
                         array_push($options_with_rfo,$terms->term_id);
-                        
+
 
                     }
-                    
+
                   }
                 }
                 // else
@@ -58,7 +56,7 @@ if(isset($_POST['getLocationsWithRFO']) && $_POST['rfo'] == 1)
 
               wp_reset_query();
 
-              
+
                   $new_arr = array();
                   for($i=0;$i <= sizeof($options_with_rfo); $i++){
                     $term =get_term($options_with_rfo[$i],'location');
@@ -73,11 +71,11 @@ if(isset($_POST['getLocationsWithRFO']) && $_POST['rfo'] == 1)
                   foreach ($sorted_terms as $terms) {
                     if($terms['id'] != '')
                       echo '<option value="'.$terms['id'].'">'.$terms['location_name'].'</option>';
-                  }        
-}  
+                  }
+}
 else
 {
-  $location = get_terms(array('taxonomy'=>'location','hide_empty'=>false)); 
+  $location = get_terms(array('taxonomy'=>'location','hide_empty'=>false));
                   foreach ($location as $loc) {
                     if($loc->parent != 0)
                       echo '<option value="'.$loc->term_id.'">'.$loc->name.'</option>';
